3f362005d003fabd830f49c012a160f32059eb10
JuanCor <36190310+JuanCor@users.noreply.github.com>
2018-04-03 10:54:20 +0200

    p1.3

--- p1.3IST/src/Servlets/Usuario.java
+++ p1.3IST/src/Servlets/Usuario.java
@@ -2,43 +2,25 @@ package Servlets;
 
 public class Usuario {
 	private  String usuario;
-	private  int clave;
-	
+	private  int clave;	
 	public Usuario (String string, String string2, String string3) {
 		this.usuario = usuario;
 		this.clave = 12345;
 	}
-	
-	
-
 	public String getUsuario() {
 		return usuario;
 	}
-
-
-
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
 	}
-
-
-
 	public int getClave() {
 		return clave;
 	}
-
-
-
 	public void setClave(String clave) {
 		this.clave = 12345;
 	}
-
-
-
 	public boolean checkAdmin(String usuario, String passw) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-
 }
